11e17f851d747f1def276ecd01dbad8f0791e1ab
CharlesRajaR <rcr2003rcharlesraja@gmail.com>
2025-04-21 08:56:37 +0530

    backend connected to koyeb

--- src/component/config/api.js
+++ src/component/config/api.js
@@ -1,12 +1,12 @@
 import axios from "axios"
 
-export const API_URL = "https://artistic-orel-rcraja-735c7b5f.koyeb.app/"
+export const API_URL = "https://artistic-orel-rcraja-735c7b5f.koyeb.app"
 
 export const api = axios.create({
     baseURL:API_URL,
     headers:{
         "Content-Type":"application/json",
     }
-})
+});
 
 
